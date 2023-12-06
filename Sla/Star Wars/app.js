484ca14471cbca79aa495e72aed2d54f35c9526b
Shadowind201 <142364452+Shadowind201@users.noreply.github.com>
2023-12-06 20:06:19 -0300

    star wars

---
+++ Sla/Star Wars/app.js
@@ -0,0 +1,12 @@
+function jedi(){
+    document.getElementById("mensagem").innerText = "Venha ao lado da luz"
+    document.getElementById("frase").innerText = "Guerras não fazem ninguém maior"
+    document.getElementById("imagem").src="jedi.gif"
+    document.body.style="background: #607479"
+}
+function sith(){
+    document.getElementById("mensagem").innerText = "Venha para o lado sombrio"
+    document.getElementById("frase").innerText = "A paz é uma mentira, só a emoção importa"
+    document.getElementById("imagem").src="sith.gif"
+    document.body.style="background: #4f5055"
+}
